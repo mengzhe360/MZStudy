8f81ab24f122fb65602c637c706f6be42fef99ab
zhemeng <mengzhe360@163.com>
2025-02-04 17:43:48 +0800

    【feat】添加LVPaymentModule

---
+++ MZArchitecture/LVPaymentModule/LVPaymentModule/LVPaymentModule.h
@@ -0,0 +1,18 @@
+//
+//  LVPaymentModule.h
+//  LVPaymentModule
+//
+//  Created by zhemeng on 2025/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LVPaymentModule.
+FOUNDATION_EXPORT double LVPaymentModuleVersionNumber;
+
+//! Project version string for LVPaymentModule.
+FOUNDATION_EXPORT const unsigned char LVPaymentModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LVPaymentModule/PublicHeader.h>
+
+
